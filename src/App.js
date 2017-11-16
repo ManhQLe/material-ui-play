a3d04400b6afb47b7a09091b91b7ff70a6620497
Manh Le <quang.manh@gmail.com>
2017-11-15 21:34:55 -0600

    Pad out content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {	Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import RaisedButtonExp from './components/1.RaisedButtonExp'
 import AppBarExp from './components/2.AppBarExp'
@@ -6,10 +6,12 @@ import AppBarExp from './components/2.AppBarExp'
 class App extends Component {
 	render() {
 		return (
-			<div>				
-				<AppBarExp/>
-				<RaisedButtonExp/>
-				<div style={{height:1400}}>
+			<div>
+				<AppBarExp />
+				<div style={{paddingTop:"80px"}}>
+					<RaisedButtonExp />
+					<div style={{ height: 1400 }}>
+					</div>
 				</div>
 			</div>
 		);
--- src/components/2.AppBarExp.js
+++ src/components/2.AppBarExp.js
@@ -4,9 +4,19 @@ import AppBar from 'material-ui/AppBar'
 import FontIcon from 'material-ui/FontIcon';
 
 class AppBarExp extends Component{
+
+    onTitleClick = ()=>{
+        alert("On title click")
+    }
+
+    onLeftIconClick=()=>{
+        alert("Left Icon click")
+    }
     render(){
         return <AppBar title="My Appbar" style={{position:"fixed"}}
         iconElementRight={<FontIcon className="muidocs-icon-action-home"/>}
+        onTitleTouchTap = {this.onTitleClick}
+        onLeftIconButtonTouchTap={this.onLeftIconClick}
         />
     }
 }
